cd1fd29ff070720cd5cbd95cac3705657a997a60
kimgyull <mkht0210@gmail.com>
2022-12-29 21:24:43 +0900

    test commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   ],
   theme: {
     extend: {
-      'sans': ["Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", 
+      'sans': ["Pretendard Variable", "Pretendard", "-apple-system", "BlinkMacSystemFont", "system-ui", "Roboto", "Helvetica Neue", "Segoe UI", 
       "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"]
     },
   },
