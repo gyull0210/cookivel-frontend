8bd32d242ef717ecb5a11d26ffbb2da936ded38a
kimgyull <mkht0210@gmail.com>
2022-12-29 23:49:59 +0900

    add GlobalStyles.js for twin.macro

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '../styles/globals.css'
+import { cache } from '@emotion/css'
+import { CacheProvider } from '@emotion/react'
+import GlobalStyles from './../styles/GlobalStyles'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }) => (
+  <CacheProvider value={cache}>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </CacheProvider>
+)
+
+export default App
